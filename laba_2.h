ed75c786f7776041c25bdd82811d748e307ed38b
SoyRitchie <79585450+SoyRitchie@users.noreply.github.com>
2021-06-01 02:45:43 +0300

    Create laba_2.h

---
+++ laba_2.h
@@ -0,0 +1,16 @@
+#ifndef LAB2
+#define LAB2
+
+/// <summary>
+/// Lab 2, Task 1: Geometric figure. 
+/// My variant 29 -> variant of the task 5
+/// </summary>
+void Lab_2_1();
+
+/// <summary>
+/// Lab 2, Task 2: Formula. 
+/// My variant 29 -> variant of the task 2
+/// </summary>
+void Lab_2_2();
+
+#endif
