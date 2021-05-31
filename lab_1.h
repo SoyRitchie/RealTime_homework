2783cda0c7a70f0e1c18cc06013b180caa356497
SoyRitchie <79585450+SoyRitchie@users.noreply.github.com>
2021-06-01 02:43:52 +0300

    Create lab_1.h

---
+++ lab_1.h
@@ -0,0 +1,15 @@
+#ifndef LAB1
+#define LAB1
+
+/// <summary>
+/// Lab 1, Task 1: Triangle (from example).
+/// </summary>
+void Lab_1_1();
+
+/// <summary>
+/// Lab 1, Task 2: Formula. 
+/// My variant 29 -> variant of the task 5
+/// </summary>
+void Lab_1_2();
+
+#endif
